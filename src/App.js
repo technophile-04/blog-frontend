31ae435ec031dfc4f665e1bb5c9998d8b7d594fb
Shiv Bhonde <shivbhonde04@gmail.com>
2021-12-06 20:02:02 +0530

    move register to pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Register from './components/Register';
+import Register from './pages/Register';
 import Home from './pages/Home';
 
 function App() {
